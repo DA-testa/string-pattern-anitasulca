d6b7629e26a810261ec0e0069d5efda90a2735b0
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-04-15 10:37:45 +0000

    tests

--- hash_substring.py
+++ hash_substring.py
@@ -1,76 +1,58 @@
 #python3
 
-class RabinKarp:
-    def __init__(self, pattern, text):
-        self.pattern = pattern
-        self.text = text
-        self.prime = 101
-        self.multiplier = 256
-        self.bucket_count = len(text) - len(pattern) + 1
-        self.buckets = [[] for _ in range(self.bucket_count)]
+def read_input():
+    # this function needs to acquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    input_type = input().rstrip().upper()
 
-    def _hash_func(self, s):
-        # Hash function
-        ans = 0
-        for c in reversed(s):
-            ans = (ans * self.multiplier + ord(c)) % self.prime
-        return ans % self.bucket_count
+    if input_type == 'I':
+        # Read input from keyboard
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        # Read input from file
+        with open('test_sample.txt', 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
 
-    def add(self, string):
-        hashed = self._hash_func(string)
-        bucket = self.buckets[hashed]
-        if string not in bucket:
-            self.buckets[hashed].append(string)
+    # Return both lines as a tuple
+    return pattern, text
 
-    def delete(self, string):
-        hashed = self._hash_func(string)
-        bucket = self.buckets[hashed]
-        for i in range(len(bucket)):
-            if bucket[i] == string:
-                bucket.pop(i)
-                break
+def print_occurrences(output):
+    # this function should control output, it doesn't need any return
+    print(' '.join(map(str, output)))
 
-    def find(self, string):
-        hashed = self._hash_func(string)
-        if string in self.buckets[hashed]:
-            return "yes"
-        else:
-            return "no"
-
-
-def rabin_karp(pattern, text):
+def get_occurrences(pattern, text):
+    # this function should find the occurrences using Rabin-Karp algorithm 
     occurrences = []
     p_len = len(pattern)
     t_len = len(text)
-    p_hash = hash(pattern)
-    t_hash = hash(text[:p_len])
+
+    # Compute the hash value of the pattern and the first substring of text
+    p_hash = sum(ord(pattern[i]) for i in range(p_len)) % (2**64)
+    t_hash = sum(ord(text[i]) for i in range(p_len)) % (2**64)
+
+    # Precompute the power of prime for rolling hash
+    prime = 101
+    prime_power = 1
+    for _ in range(p_len):
+        prime_power = (prime_power * prime) % (2**64)
 
     for i in range(t_len - p_len + 1):
         if p_hash == t_hash:
             if pattern == text[i:i + p_len]:
                 occurrences.append(i)
-
+        
         if i < t_len - p_len:
-            t_hash = hash(text[i + 1:i + p_len + 1])
+            # Update the rolling hash value for the next substring
+            t_hash = (t_hash - ord(text[i])) % (2**64)
+            t_hash = (t_hash * prime + ord(text[i + p_len])) % (2**64)
+            if t_hash < 0:
+                t_hash += 2**64
 
+    # Return an iterable variable
     return occurrences
 
-
+# This part launches the functions
 if __name__ == '__main__':
-    input_type = input().rstrip().upper()
-
-    if input_type == 'I':
-        pattern = input().rstrip()
-        text = input().rstrip()
-    elif input_type == 'F':
-        with open('test_sample.txt', 'r') as file:
-            pattern = file.readline().rstrip()
-            text = file.readline().rstrip()
-
-    occurrences = rabin_karp(pattern, text)
-    print_occurrences(occurrences)
-
-
-
-
-
+    print_occurrences(get_occurrences(*read_input()))
