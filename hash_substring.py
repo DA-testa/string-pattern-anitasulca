a2e1c1ee7728255414984143ad02128cd3436468
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-04-16 17:59:15 +0000

    tests

--- hash_substring.py
+++ hash_substring.py
@@ -18,28 +18,34 @@ def read_input():
     # Return both lines as a tuple
     return pattern, text
 
+
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
+
 
 def get_occurrences(pattern, text):
     # this function should find the occurrences using Rabin-Karp algorithm 
     occurrences = []
     p_len = len(pattern)
     t_len = len(text)
-    p_hash = hash(pattern)
-    t_hash = hash(text[:p_len])
 
+    # Compute hash of the pattern and the initial hash of the first window in the text
+    p_hash = sum(ord(c) for c in pattern)
+    t_hash = sum(ord(c) for c in text[:p_len])
+
+    # Iterate through all possible windows in the text
     for i in range(t_len - p_len + 1):
-        if p_hash == t_hash:
-            if pattern == text[i:i + p_len]:
+        if p_hash == t_hash:  # If the hash of the window matches the hash of the pattern
+            if pattern == text[i:i + p_len]:  # If the window matches the pattern
                 occurrences.append(i)
-        
-        if i < t_len - p_len:
-            t_hash = hash(text[i + 1:i + p_len + 1])        
+
+        if i < t_len - p_len:  # Update the hash for the next window
+            t_hash = t_hash - ord(text[i]) + ord(text[i + p_len])
 
     # Return an iterable variable
     return occurrences
+
 
 # This part launches the functions
 if __name__ == '__main__':
